eba158a426f8ef7cd90f00c301070937fb41f988
omkarjadhav21 <omkarjadhav7110@gmail.com>
2017-12-07 18:02:00 +0530

    Ad Customer Profile Pic

--- src/app/add-custmomer/add-custmomer.component.ts
+++ src/app/add-custmomer/add-custmomer.component.ts
@@ -10,13 +10,14 @@ import { Validators } from '@angular/forms';
   styleUrls: ['./add-custmomer.component.css']
 })
 export class AddCustmomerComponent implements OnInit {
-
-  addUserFrm: FormGroup;
+  ImgUrl: any;
+  
+  userRegisterFrm: FormGroup;
   constructor(private fb: FormBuilder,
               private LoginService:LoginService) { }
 
   ngOnInit() {
-    this.addUserFrm = this.fb.group({
+    this.userRegisterFrm = this.fb.group({
       email: ['', Validators.compose([
         Validators.required,
         Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]
@@ -40,10 +41,22 @@ export class AddCustmomerComponent implements OnInit {
   length10(control: AbstractControl): ValidationErrors | null {
     return control.value.length >= 10 ? null : { myErr: 'Number must be 10 Digits' };
   }
-
+  choosePhoto() {
+    document.getElementById('my_file').click();
+  }
+  readUrl(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+      reader.onload = (event: any) => {
+        this.ImgUrl = event.target.result;
+      }
+      reader.readAsDataURL(event.target.files[0]);
+      console.log(event.target.files[0].name);
+    }
+  }
 
   submitUser() {
-    this.LoginService.addCustservice(this.addUserFrm.value).subscribe(data=>{
+    this.LoginService.addCustservice(this.userRegisterFrm.value).subscribe(data=>{
       console.log(data);  
     })
   }
